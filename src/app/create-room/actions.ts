2baf2d5f58196e574053929920aad5a4d106b012
novanishant <nishantverma3633@gmail.com>
2024-04-12 11:40:10 +0530

    display all rooms on mainpage

--- src/app/create-room/actions.ts
+++ src/app/create-room/actions.ts
@@ -1,12 +1,14 @@
-"use server"
+"use server";
 
-import { Room,room } from "@/db/schema";
-import {db} from "@/db";
-import {getSession} from "@/lib/auth";
-export async function createRoomAction(roomData : Omit<Room,"id"|"userId">){
-    const session  = await getSession();
-    if(!session){
-        throw new Error("you must be logged in to create this room");
-    }
-    await db.insert(room).values({...roomData,userId:session.user.id});
+import { Room, room } from "@/db/schema";
+import { db } from "@/db";
+import { getSession } from "@/lib/auth";
+import { revalidatePath } from "next/cache";
+export async function createRoomAction(roomData: Omit<Room, "id" | "userId">) {
+  const session = await getSession();
+  if (!session) {
+    throw new Error("you must be logged in to create this room");
+  }
+  await db.insert(room).values({ ...roomData, userId: session.user.id });
+  revalidatePath("/");
 }
