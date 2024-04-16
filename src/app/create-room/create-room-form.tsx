bc6f5980a26d3b44d4f027e3aaf12d309f56af34
novanishant <nishantverma3633@gmail.com>
2024-04-16 15:15:47 +0530

    gitRepo maxchar size increased

--- src/app/create-room/create-room-form.tsx
+++ src/app/create-room/create-room-form.tsx
@@ -20,7 +20,7 @@ import { toast } from "@/components/ui/use-toast";
 const formSchema = z.object({
   name: z.string().min(2).max(50),
   description: z.string().min(2).max(100),
-  githubRepo: z.string().min(2).max(50),
+  githubRepo: z.string().min(2).max(100),
   tags: z.string().min(1).max(50),
 });
 
--- src/app/edit-room/[roomId]/edit-room-form.tsx
+++ src/app/edit-room/[roomId]/edit-room-form.tsx
@@ -22,7 +22,7 @@ import { toast } from "@/components/ui/use-toast";
 const formSchema = z.object({
   name: z.string().min(1).max(50),
   description: z.string().min(1).max(250),
-  githubRepo: z.string().min(1).max(50),
+  githubRepo: z.string().min(1).max(100),
   tags: z.string().min(1).max(50),
 });
 
